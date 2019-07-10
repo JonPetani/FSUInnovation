2607b05c87a2349af5512c303260359420ab0d21
JonPetani <49282269+JonPetani@users.noreply.github.com>
2019-07-10 16:59:10 -0400

    Login and Keyword Search Changes

--- Keywords.php
+++ Keywords.php
@@ -1,5 +1,8 @@
 <?php
 	$con = new PDO('mysql:host=localhost:3306;dbname=internsite;charset=utf8mb4','SiteAdmin','fsuintern495');
 	$keyword = $_POST['Keyword'];
-	$results = $con -> query("SELECT * FROM member WHERE ");
+	$results = $con -> query("SELECT * FROM member WHERE keyword.Keyword = $keyword AND keyword.CompanyName = member.CompanyName");
+	while(){
+	}
+	
 ?>
--- MemberCheck.php
+++ MemberCheck.php
@@ -1,30 +1,82 @@
 <?php
    ob_start();
    session_start();
-   if ( ! empty( $_POST ) ) {
-    if ( isset( $_POST['Username'] ) && isset( $_POST['Password'] ) ) {
-        // Getting submitted user data from database
-        $con = new PDO('mysql:host=localhost:3306;dbname=internsite;charset=utf8mb4','SiteAdmin','fsuintern495');
-        $stmt = $con->prepare("SELECT * FROM member WHERE Username = ?");
-        $stmt->execute();
-        $result = $stmt->fetchColumn();
-    	$user = $result->fetch_object();
-    		
-    	// Verify user password and set $_SESSION
-    	if ( password_verify( $_POST['Password'], $user->password ) ) {
-    		$_SESSION['MemberId'] = $user->ID;
-			
-    	}
-		else {
-			header ("location: FailedLoginMember.html");
-		}
+   $con = new PDO('mysql:host=localhost:3306;dbname=internsite;charset=utf8mb4','SiteAdmin','fsuintern495');
+   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: LoggedInMember.php");
+    exit;
     }
-		else {
-			header ("location: FailedLoginMember.html");
-		}
+      $username = $password = "";
+$username_err = $password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, username, password FROM users WHERE username = :username";
+        
+        if($stmt = $pdo->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            
+            // Set parameters
+            $param_username = trim($_POST["username"]);
+            
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Check if username exists, if yes then verify password
+                if($stmt->rowCount() == 1){
+                    if($row = $stmt->fetch()){
+                        $id = $row["id"];
+                        $username = $row["username"];
+                        $hashed_password = $row["password"];
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+                            // Redirect user to welcome page
+                            header("location: welcome.php");
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = "The password you entered was not valid.";
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = "No account found with that username.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        unset($stmt);
+    }
+    
+    // Close connection
+    unset($pdo);
 }
-else {
-		header ("location: FailedLoginMember.html");
-	 }
-?>
 ?>
